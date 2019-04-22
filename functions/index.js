0b129ca316f35026f71e229ef6f116a85c17b492
Will C <will@quid.works>
2019-04-21 21:03:43 -0400

    Use idTokens instead of uids

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,12 @@
 const functions = require('firebase-functions');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
 // 1. create firestore
 // 2. download firebase key file
-  // firebase settings > service accounts
+// firebase settings > service accounts
 // 3. download google api secrets
-  // Go to https://console.cloud.google.com/apis/credentials?project=oauth-flows&authuser=3&organizationId=598037646388
-  // Go to client ID for web app
-  // Copy client secret and client id (download JSON)
+// Go to https://console.cloud.google.com/apis/credentials?project=oauth-flows&authuser=3&organizationId=598037646388
+// Go to client ID for web app
+// Copy client secret and client id (download JSON)
 // 4. init clients (firebase and google apis)
 // 5. exchange code for tokens
 // 6. Save refresh token.
@@ -25,11 +19,13 @@ const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 const { google } = require('googleapis');
 
+// https://console.cloud.google.com/apis/credentials/oauthclient/
 const googleSecrets = require("./google-secrets.json");
+// https://console.cloud.google.com/apis/credentials/oauthclient/
 const serviceAccount = require("./oauth-flows-service-key.json");
 
-const clientId = googleSecrets.web.client_id
-const clientSecret = googleSecrets.web.client_secret
+const clientId = googleSecrets.web.client_id;
+const clientSecret = googleSecrets.web.client_secret;
 // Don't use an actual redirect uri from our list of valid uri's. Instead, it needs to be postmessage.
 // https://stackoverflow.com/a/48121098/7621726
 const redirectUri = 'postmessage'
@@ -45,7 +41,7 @@ const app = admin.initializeApp({
   databaseURL: "https://oauth-flows.firebaseio.com"
 });
 
-// Wrap callback in a promise.
+// Helpers
 const getToken = (code) => {
   return new Promise((res, rej) => {
     oauth2Client.getToken(code, (err, token) => {
@@ -56,54 +52,130 @@ const getToken = (code) => {
   })
 }
 
-exports.offlineGrant = functions.https.onRequest(async (request, response) => {
-  const { code, uid } = request.query
+const extractUid = async (idToken) => {
+  const userInfo = await app.auth().verifyIdToken(idToken)
+  console.log(idToken)
+  return userInfo.uid
+}
 
+exports.offlineGrant = functions.https.onRequest(async (request, response) => {
+  const { code, idToken } = request.query
+  response.set('Access-Control-Allow-Origin', '*');
+  console.log('offlineGrant idToken:', idToken, 'code:', code)
   if (!code) {
     response.status(400).send('Missing auth code')
     return
   }
-  if (!uid) {
-    response.status(400).send('Missing uid')
+  if (!idToken) {
+    response.status(400).send('Missing idToken')
     return
   }
+
+  let uid
+  try {
+    uid = await extractUid(idToken)
+  } catch (err) {
+    response.status(400).send('Error verifying id token')
+    return
+  }
+
+  console.log('uid', uid)
 
   const token = await getToken(code)
 
   // Overwrite previous value.
   await app.firestore().collection("users").doc(uid).set({ refreshToken: token.refresh_token })
-  console.log('save refresh token in user doc')
+  console.log('save refresh token in user doc with uid', uid)
   response.send(token)
 
 })
 
 exports.events = functions.https.onRequest(async (request, response) => {
-  const { uid } = request.query
-
-  if (!uid) {
-    response.status(400).send('Missing uid')
+  let refreshToken
+  const { idToken } = request.query
+  refreshToken = request.query.refreshToken
+  response.set('Access-Control-Allow-Origin', '*');
+  if (!idToken) {
+    response.status(400).send('Missing id token')
     return
   }
 
-  const user = await app.firestore().collection("users").doc(uid).get()
+  let uid
+  try {
+    uid = await extractUid(idToken)
+  } catch (err) {
+    response.status(400).send('Error verifying id token')
+    return
+  }
 
-  const refreshToken = user.data().refreshToken
+  console.log('uid', uid)
+  const user = await app.firestore().collection("users").doc(uid).get();
+  console.log(user)
+  if (!user.exists) {
+    response.status(400).send('No credentials saved for this user.')
+    return
+  }
 
+  if (!refreshToken) {
+    refreshToken = user.data().refreshToken
+  }
+
+  console.log('refresh token', refreshToken)
   oauth2Client.setCredentials({ refresh_token: refreshToken })
 
-  // https://github.com/googleapis/google-api-nodejs-client to find your api.
+  // https://github.com/googleaps/google-api-nodejs-client to find your api.
   // If using VS Code, the intellisense is magical.
   const calendarApiClient = google.calendar({
-    version: 'v3' ,
+    //   // For development only, we want to restrict this to allowed origins.
+    //   response.set('Access-Control-Allow-Origin', '*');
+    //   response.send(events.data)
+    // })
+    version: 'v3',
     auth: oauth2Client,
   })
 
-
-  const events = await calendarApiClient.events.list({
-    calendarId: 'primary'
-  })
+  let events
+  try {
+    events = await calendarApiClient.events.list({
+      calendarId: 'primary'
+    })
+  } catch (err) {
+    console.error(err)
+  }
 
   // For development only, we want to restrict this to allowed origins.
-  response.set('Access-Control-Allow-Origin', '*');
   response.send(events.data)
+})
+
+exports.tokens = functions.https.onRequest(async (request, response) => {
+  const { idToken } = request.query
+  response.set('Access-Control-Allow-Origin', '*');
+  console.log('tokens, idToken:', idToken)
+  if (!idToken) {
+    response.status(400).send('Missing id token')
+    return
+  }
+
+  let uid
+  try {
+    uid = await extractUid(idToken)
+  } catch (err) {
+    response.status(400).send('Error verifying id token')
+    return
+  }
+
+  console.log('uid', uid)
+
+  const user = await app.firestore().collection("users").doc(uid).get()
+  console.log(user)
+  if (!user.exists) {
+    response.status(400).send('No credentials saved for this user.')
+    return
+  }
+
+  const refreshToken = user.data().refreshToken
+  console.log('refresh token', refreshToken)
+  oauth2Client.setCredentials({ refresh_token: refreshToken })
+  const accessToken = await oauth2Client.getAccessToken()
+  response.send({ accessToken: accessToken.token })
 })
